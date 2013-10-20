f1414ef77790e2fb24196c6b61e926b07303fd99
Carlos Mendes <cmendesce@gmail.com>
2013-10-20 12:47:17 -0300

    Added some examples.

---
+++ NSDate-Example/NSDate-Example/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  NSDate-Example
+//
+//  Created by Carlos Mendes on 20/10/13.
+//  Copyright (c) 2013 Carlos Mendes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITableViewController
+
+@end
---
+++ NSDate-Example/NSDate-Example/HomeViewController.m
@@ -0,0 +1,68 @@
+//
+//  HomeViewController.m
+//  NSDate-Example
+//
+//  Created by Carlos Mendes on 20/10/13.
+//  Copyright (c) 2013 Carlos Mendes. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "NSDate+Helper.h"
+
+@interface HomeViewController ()
+
+@property (strong, nonatomic) NSMutableArray *dates;
+
+@end
+
+@implementation HomeViewController
+
+-(NSMutableArray *)dates {
+    if (!_dates) {
+        _dates = [[NSMutableArray alloc] init];
+    }
+    return _dates;
+}
+
+- (id)init:(UITableViewStyle)style
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    self.title = @"NSDate+Helper Examples";
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSDate *now = [NSDate date];
+    
+    [self.dates addObject:[now stringWithFormat:@"dd-MM-yyyy"]];
+    [self.dates addObject: [NSString stringWithFormat:@"%i",[now daysAgo]]];
+    [self.dates addObject: [NSString stringWithFormat:@"%i",[now daysAgoAgainstMidnight]]];
+    [self.dates addObject:[now stringDaysAgo]];
+    //[self.dates addObject:[NSDate dateFromString:@"12-12-2013" withFormat:@"dd-MM-yyyy"]];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self dates].count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = self.dates[indexPath.row];
+    
+    return cell;
+}
+
+@end
